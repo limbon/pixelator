28511c3d3677cbf0b5157265933c85891fad406e
= <=>
2020-04-25 13:40:12 +0600

    fix tools not being selected properly

--- src/store/ToolStore.ts
+++ src/store/ToolStore.ts
@@ -1,10 +1,15 @@
-import { observable, action } from 'mobx';
+import { observable, action, computed } from 'mobx';
 import { Tool } from '../types';
 import { pencil, bucket } from '../tools';
 
 export class ToolStore {
 	@observable tools: Tool[] = [pencil, bucket];
 	@observable selectedTool: Tool = this.tools[0];
+
+	// @computed
+	// get SelectedTool() {
+	// 	return this.selectedTool;
+	// }
 
 	@action
 	select(idx: number) {
--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,8 @@ module.exports = {
 				query: {
 					presets: ['@babel/env', '@babel/preset-typescript'],
 					plugins: [
-						['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
-						'@babel/plugin-proposal-class-properties',
+						['@babel/plugin-proposal-decorators', { legacy: true }],
+						['@babel/plugin-proposal-class-properties', { loose: true }],
 					],
 				},
 			},
